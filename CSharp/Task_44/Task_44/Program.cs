ed17e1036718e19dabb5059849dd79782d9715e8
Wisdou <51473681+Wisdou@users.noreply.github.com>
2022-02-16 23:53:43 +0300

    Add task 44 for C#

---
+++ CSharp/Task_44/Task_44/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_44
+{
+    class Program
+    {
+        static long GetMinDifference()
+        {
+            HashSet<long> hashset = new HashSet<long>();
+            for (long i = 1; ; i++)
+            {
+                long numb = (i * (3 * i - 1)) / 2;
+                hashset.Add(numb);
+                for (long j = i - 1; j >= 1; j--)
+                {
+                    long sum2 = (j * (3 * j - 1)) / 2;
+                    if (hashset.Contains(numb - sum2) && hashset.Contains(numb + sum2))
+                    {
+                        return numb - sum2;
+                    }
+                }
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMinDifference());
+        }
+    }
+}
